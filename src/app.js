250ab5ed1bfbbb91cad566be29e5ddc9d306af3c
Luis <33837673+lfresnog@users.noreply.github.com>
2019-09-27 15:25:48 +0200

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,62 @@
+import yargs from 'yargs';
+import {add,list,remove,read} from './utils';
+
+
+//Add
+yargs.command({
+    command:'add',
+    describe:'Add a new note',
+    builder:{
+        title:{
+            describe:'Title of the note',
+            demandOption:true,
+            type:'string',
+        },
+        body: {
+            describe: 'body of the note',
+            demandOption: true,
+            type: 'string',
+          },
+          author: {
+            describe: 'body of the note',
+            demandOption: true,
+            type: 'string',
+          },
+    },
+    handler: add,
+})
+
+//List
+yargs.command({
+    command:'list',
+    describe:'List titles of notes',
+    handler: list,
+})
+
+//Remove
+yargs.command({
+  command:'remove',
+  describe: 'Remove a note',
+  index: {
+    describe: 'Info necessary to choose the note to remove',
+    demandOption: true,
+    type: 'number',
+  },
+  handler: remove,
+
+})
+
+//Read
+yargs.command({
+  command:'read',
+  describe: 'Read a note',
+  index: {
+    describe: 'Info necessary to choose the note to read',
+    demandOption: true,
+    type: 'number',
+  },
+  handler: read,
+
+})
+
+yargs.parse();
---
+++ src/utils.js
@@ -0,0 +1,62 @@
+import uuidv1 from 'uuid/v1';
+import  fs from 'fs';
+
+//Notes
+const book={
+    notes:[]
+}
+
+function add(argv){
+    const note={
+        title: argv.title,
+        body: argv.body,
+        author: argv.author,
+        id:uuidv1()
+    }
+    const path = './notes.txt';
+    fs.access(path, fs.F_OK, (err) => {
+        if(err){
+        book.notes.push(note);
+        const str=JSON.stringify(book);
+        fs.writeFileSync("notes.txt",str);
+        }
+        else{
+            const str1=fs.readFileSync('notes.txt').toString()
+            const book=JSON.parse(str1);
+            book.notes.push(note);
+            const str2 = JSON.stringify(book);
+            fs.writeFileSync("notes.txt",str2);
+        }
+        
+    });
+   
+}
+
+function list(){
+    const str=fs.readFileSync('notes.txt').toString()
+    const book=JSON.parse(str);
+    book.notes.forEach( (note, i) => {
+    console.log(`${i}: ${note.title}`);
+  })
+}
+function remove(argv){
+    const str=fs.readFileSync('notes.txt').toString()
+    const book=JSON.parse(str);
+    book.notes.splice(argv.index-1,1);
+    const str2 = JSON.stringify(book);
+    fs.writeFileSync("notes.txt",str2);
+    
+    
+}
+function read(argv){
+    const str=fs.readFileSync('notes.txt').toString()
+    const book=JSON.parse(str);
+    try {
+        console.log(book.notes[argv.index].body);
+      }
+      catch(error) {
+        console.error("That note doesn't exist");
+      }
+}
+
+export{add,list,remove,read};
