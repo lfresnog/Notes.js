7aaad6be67c8e78f65d82b58b164c373a1313719
Luis <33837673+lfresnog@users.noreply.github.com>
2019-10-02 12:12:34 +0200

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,8 +1,5 @@
 import yargs from 'yargs';
-import  fs from 'fs';
-import {add,list,remove,read} from './utils';
-
-let book;
+import {add,list,remove,read,main} from './utils';
 
 //Add
 yargs.command({
@@ -61,21 +58,7 @@ yargs.command({
 
 })
 
-export{book};
+main();
 
-const path = './notes.txt';
 
-fs.access(path, fs.F_OK, (err) => {
-  if (err) {
-    fs.writeFileSync("notes.txt","");
-  }
-  const data = fs.readFileSync("notes.txt").toString();
-  if(data !== ""){
-     book = JSON.parse(data);
-  }else{
-     book = {notes: []};
-  }
-  yargs.parse();
-  fs.writeFileSync("notes.txt", JSON.stringify(book));
-});
 
--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,10 @@
 import uuidv1 from 'uuid/v1';
 import chalk from 'chalk';
-import {book} from './app';
+import  fs from 'fs';
+import yargs from 'yargs';
 
 //Notes
-
+let book;
 
 const add = function add(argv){
     const note={
@@ -18,18 +19,28 @@ const add = function add(argv){
 
 const list = function list(){
     try {
-        book.notes.forEach( (note, i) => {
+      if (array.length == 0) {
+        console.error(chalk.red("\n Array empty \n"));
+    }else{
+      book.notes.forEach( (note, i) => {
         console.log(`${i}: ${note.title}`);
       })
+    } 
       }
       catch(error) {
         console.error(chalk.red("\n There aren't any notes \n"));
       }
     
 }
+
 const remove = function remove(argv){
     try {
-        book.notes.splice(argv.index,1);
+      if (array.length == 0) {
+        console.error(chalk.red("\n Array empty \n"));
+    }
+    else{
+      book.notes.splice(argv.index,1);
+    } 
       }
       catch(error) {
         console.error(chalk.red("\n There aren't any notes \n"));
@@ -38,6 +49,7 @@ const remove = function remove(argv){
     
     
 }
+
 const read = function read(argv){
     try {
         console.log("\n"+ chalk.blue(book.notes[argv.index].title));
@@ -49,4 +61,21 @@ const read = function read(argv){
       }
 }
 
-export{add,list,remove,read};
+const main = function main(){
+  const path = './notes.txt';
+fs.access(path, fs.F_OK, (err) => {
+  if (err) {
+    fs.writeFileSync("notes.txt","");
+  }
+  const data = fs.readFileSync("notes.txt").toString();
+  if(data !== ""){
+     book = JSON.parse(data);
+  }else{
+     book = {notes: []};
+  }
+  yargs.parse();
+  fs.writeFileSync("notes.txt", JSON.stringify(book));
+});
+}
+
+export{add,list,remove,read,main};
